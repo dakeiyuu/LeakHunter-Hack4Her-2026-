8842c105cfcb674dde468d7e31606a5a6afa0873
HM <108926387+HPMM2@users.noreply.github.com>
2026-06-07 03:38:19 -0600

    Add files via upload

---
+++ LeakHunter-Hack4Her-2026--main 2/Churn/Churn/ChurnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChurnApp.swift
+//  Churn
+//
+//  Created by Dania Ortiz on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ChurnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}
